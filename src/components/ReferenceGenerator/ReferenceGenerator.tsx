c94b31de7d299841bc085d1c37cd4904949972a8
Harshtandel95 <harsh@markweb.in>
2025-05-05 11:44:27 +0530

    Update ReferenceGenerator.tsx

--- src/components/ReferenceGenerator/ReferenceGenerator.tsx
+++ src/components/ReferenceGenerator/ReferenceGenerator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { ClipboardDocumentIcon } from "@heroicons/react/24/outline";
 
 const ReferenceGenerator = () => {
